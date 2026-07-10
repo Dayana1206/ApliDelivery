14ae19b000576b2bfd900fa713f9c5a839a442aa
JOEL STEVEN PUMA CRIOLLO <jspumac@utn.edu.ec>
2026-07-10 12:28:34 -0500

    feat: completar interfaz web de administrador y cliente

---
+++ ApliDelivery.Web/Controllers/RestauranteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApliDelivery.Web.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult Detalle()
+        {
+            return View();
+        }
+    }
+}
